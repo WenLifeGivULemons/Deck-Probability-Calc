b71c9c7b426af6d33f2684547108cbc608c817c6
Lernonad <42011225+WenLifeGivULemons@users.noreply.github.com>
2024-08-30 09:21:00 -0400

    creating groups

--- deck.py
+++ deck.py
@@ -1,9 +1,9 @@
 import card
 
-
 class Deck:
     def __init__(self, decksize):
         self.size = decksize
+        self.grouplist = []
         # create the cards in the deck
         self.cardlist = []
         i = 0
@@ -29,4 +29,18 @@ class Deck:
         while i < self.size:
             print(str(i + 1) + ": " + self.cardlist[i].name)
             i += 1
+        pass
+
+    def create_group(self):
+        group = []
+        # TODO: complete
+        self.grouplist.append(group)
+        pass
+
+
+    def list_groups(self):
+        for i in range(len(self.grouplist)):
+            # TODO: test after completing above function
+            print(self.grouplist[i])
+            pass
         pass
